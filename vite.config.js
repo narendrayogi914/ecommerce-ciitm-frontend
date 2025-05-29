148f54314c48b46e53acd3db01b8ba03bd15bdf3
narendrayogi914 <narendrayogi914@gmail.com>
2025-05-29 20:35:00 +0530

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import path from "path"
+import path from "path";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // optional for dev server
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
